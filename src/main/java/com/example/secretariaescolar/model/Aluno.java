33486fe08e8946869993a5314eb2ed56c8394e0e
GuedesTech <guilhermeguedes350@gmail.com>
2026-02-03 14:50:53 -0300

    Adicionei as classes Aluno e Usuario

---
+++ src/main/java/com/example/secretariaescolar/model/Aluno.java
@@ -0,0 +1,21 @@
+package com.example.secretariaescolar.model;
+
+public class Aluno {
+    private int idAluno;
+    private int matricula;
+    private int idTurma;
+
+    public Aluno(int idAluno, int matricula, int idTurma) {
+        this.idAluno = idAluno;
+        this.matricula = matricula;
+        this.idTurma = idTurma;
+    }
+
+    public int getIdAluno() { return idAluno; }
+    public int getMatricula() { return matricula; }
+    public int getIdTurma() { return idTurma; }
+
+    public void setIdAluno(int idAluno) { this.idAluno = idAluno; }
+    public void setMatricula(int matricula) { this.matricula = matricula; }
+    public void setIdTurma(int idTurma) { this.idTurma = idTurma; }
+}
---
+++ src/main/java/com/example/secretariaescolar/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.example.secretariaescolar.model;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private int id_tipoUser;
+
+    public Usuario(String nome, String email, String senha, int id_tipoUser) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.id_tipoUser = id_tipoUser;
+    }
+
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getSenha() { return senha; }
+    public int getId_tipoUser() { return id_tipoUser; }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setEmail(String email) { this.email = email; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public void setId_tipoUser(int id_tipoUser) { this.id_tipoUser = id_tipoUser; }
+}
